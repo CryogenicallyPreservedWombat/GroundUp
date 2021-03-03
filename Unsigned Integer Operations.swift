c9b58fadd449b19fda6bc50ea065575745bbaa6f
CPWombat <etanossip@icloud.com>
2021-03-03 02:21:28 -0500

    Replaced Xcode project with the swift files contained within

---
+++ Unsigned Integer Operations.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  GroundUp
+//
+//  Created by Etan Ossip on 3/3/21.
+//
+
+import Foundation
+
+// Defining operators and functions entirely from the bitwise operators: ~ & | ^ << and >> as well as the associated syntactic sugar &=, |=, <<=, and >>=
+// Unfortunately (in this context) 0 does not evaluate as false, so I will be permitting myself != 0 and == 0 as valid operations
+
+func next(_ value: UInt) -> UInt {
+    var bit: UInt = 1
+    var x = value
+    while bit & x != 0 {
+        x ^= bit
+        bit <<= 1
+    }
+    x |= bit
+    return x
+}
+
+func add(_ value1: UInt, _ value2: UInt) -> UInt {
+    var carry: UInt = 0
+    var (x, y) = (value1, value2)
+    var bit: UInt = 0
+    var ans: UInt = 0
+    while (x | y | carry) != 0 {
+        ans |= ((x & 1) ^ (y & 1) ^ carry) << bit
+        carry = ((x & 1) & (y & 1)) | ((carry) & (x & 1)) | ((carry) & (y & 1))
+        
+        bit = next(bit)
+        
+        x >>= 1
+        y >>= 1
+    }
+    
+    return ans
+}
+
+func multiply(_ value1: UInt, _ value2: UInt) -> UInt {
+    var ans: UInt = 0
+    var (x, y) = (value1, value2)
+    while x != 0 {
+        if x & 1 != 0 { ans = add(ans, y) }
+        ans <<= 1
+        x >>= 1
+    }
+    return ans
+}
